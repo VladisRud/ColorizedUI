019261bda75fa0e60f200f7c8885f1713fcca34c
Vladislav Rudenko <vladisrud@gmail.com>
2024-06-11 15:04:21 +0400

    Initial Commit

---
+++ ColorizedUI/ColorizedUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedUIApp.swift
+//  ColorizedUI
+//
+//  Created by Влад Руденко on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
